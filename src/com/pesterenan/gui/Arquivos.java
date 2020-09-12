4dcaf7decd162409e5e10e703820c923db693c70
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2020-09-12 18:28:35 -0300

    Correção do conflito gerado no ultimo merge request e adição do gitignore
    O ultimo merge request (ID: d71bd507b40ef8ab0fc495b60aafb36b2a774042) gerou um conflito não resolvido no arquivo com.pesterenan.gui.Arquivos.java

--- src/com/pesterenan/gui/Arquivos.java
+++ src/com/pesterenan/gui/Arquivos.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.pesterenan.gui;
 
 import java.io.File;
@@ -64,24 +63,24 @@ public class Arquivos {
 		linha = leitor.nextLine();
 		while (leitor.hasNextLine()) {
 			switch (linha) {
-			case DO:
-				GUI.apoastroFinalTextField.setText(retornarValor());
-				GUI.direcaoOrbitaTextField.setText(retornarValor());
-				break;
-			case SB:
-				GUI.altP.setText(retornarValor());
-				GUI.altI.setText(retornarValor());
-				GUI.altD.setText(retornarValor());
-				GUI.velP.setText(retornarValor());
-				GUI.velI.setText(retornarValor());
-				GUI.velD.setText(retornarValor());
-				break;
-			case AR:
-				GUI.nomeMarcadorTextField.setText(retornarValor());
-				GUI.velMaxTextField.setText(retornarValor());
-				break;
-			default:
-				linha = leitor.nextLine();
+				case DO:
+					GUI.apoastroFinalTextField.setText(retornarValor());
+					GUI.direcaoOrbitaTextField.setText(retornarValor());
+					break;
+				case SB:
+					GUI.altP.setText(retornarValor());
+					GUI.altI.setText(retornarValor());
+					GUI.altD.setText(retornarValor());
+					GUI.velP.setText(retornarValor());
+					GUI.velI.setText(retornarValor());
+					GUI.velD.setText(retornarValor());
+					break;
+				case AR:
+					GUI.nomeMarcadorTextField.setText(retornarValor());
+					GUI.velMaxTextField.setText(retornarValor());
+					break;
+				default:
+					linha = leitor.nextLine();
 			}
 		}
 
@@ -112,132 +111,27 @@ public class Arquivos {
 		for (String linha : linhas) {
 			if (linha.trim().contentEquals(dados[0])) {
 				switch (dados[0]) {
-				case DO:
-					linhas.set(linhas.indexOf(linha) + 1, "apoastro=" + dados[1]);
-					linhas.set(linhas.indexOf(linha) + 2, "inclinacao=" + dados[2]);
-					break;
+					case DO:
+						linhas.set(linhas.indexOf(linha) + 1, "apoastro=" + dados[1]);
+						linhas.set(linhas.indexOf(linha) + 2, "inclinacao=" + dados[2]);
+						break;
 
-				case SB:
-					linhas.set(linhas.indexOf(linha) + 1, "altp=" + dados[1]);
-					linhas.set(linhas.indexOf(linha) + 2, "alti=" + dados[2]);
-					linhas.set(linhas.indexOf(linha) + 3, "altd=" + dados[3]);
-					linhas.set(linhas.indexOf(linha) + 4, "velp=" + dados[4]);
-					linhas.set(linhas.indexOf(linha) + 5, "veli=" + dados[5]);
-					linhas.set(linhas.indexOf(linha) + 6, "veld=" + dados[6]);
-					break;
+					case SB:
+						linhas.set(linhas.indexOf(linha) + 1, "altp=" + dados[1]);
+						linhas.set(linhas.indexOf(linha) + 2, "alti=" + dados[2]);
+						linhas.set(linhas.indexOf(linha) + 3, "altd=" + dados[3]);
+						linhas.set(linhas.indexOf(linha) + 4, "velp=" + dados[4]);
+						linhas.set(linhas.indexOf(linha) + 5, "veli=" + dados[5]);
+						linhas.set(linhas.indexOf(linha) + 6, "veld=" + dados[6]);
+						break;
 
-				case AR:
-					linhas.set(linhas.indexOf(linha) + 1, "marcador=" + dados[1]);
-					linhas.set(linhas.indexOf(linha) + 2, "velocidade=" + dados[2]);
-					break;
+					case AR:
+						linhas.set(linhas.indexOf(linha) + 1, "marcador=" + dados[1]);
+						linhas.set(linhas.indexOf(linha) + 2, "velocidade=" + dados[2]);
+						break;
 				}
 			}
 		}
 		Files.write(config.toPath(), linhas);
 	}
 }
-=======
-package com.pesterenan.gui;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
-public class Arquivos {
-
-	private static File config;
-	private static File logErros;
-	private static FileWriter escritor;
-	private static Scanner leitor;
-	private static String linha;
-
-	public Arquivos() {
-		try {
-			config = new File("mp_config.cfg");
-			if (config.createNewFile()) {
-				escritor = new FileWriter(config);
-				escreverArquivoConfig();
-				escritor.close();
-				System.out.println("Arquivo de config criado.");
-			} else {
-				System.out.println("Arquivo de config já existe");
-				leitor = new Scanner(config);
-				buscarConfiguracoes();
-				leitor.close();
-			}
-
-		} catch (IOException e) {
-			System.out.println("Erro ao criar arquivo de configuração.");
-			e.printStackTrace();
-		}
-	}
-
-	private void escreverArquivoConfig() throws IOException {
-		escritor.write("[MechPeste - Configurações]\n");
-		escritor.write("[Decolagem Orbital]\n");
-		escritor.write("apoastro=80000\n");
-		escritor.write("inclinacao=90\n");
-		escritor.write("[Suicide Burn]\n");
-		escritor.write("altp=0.001\n");
-		escritor.write("alti=0.01\n");
-		escritor.write("altd=0.01\n");
-		escritor.write("velp=0.025\n");
-		escritor.write("veli=0.05\n");
-		escritor.write("veld=0.05\n");
-		escritor.write("[Auto Rover]\n");
-		escritor.write("marcador=ALVO\n");
-		escritor.write("velocidade=10\n");
-	}
-
-	private void buscarConfiguracoes() {
-		linha = leitor.nextLine();
-		while (leitor.hasNextLine()) {
-			switch (linha) {
-			case "[Decolagem Orbital]":
-				GUI.apoastroFinalTextField.setText(retornarValor());
-				GUI.direcaoOrbitaTextField.setText(retornarValor());
-				break;
-			case "[Suicide Burn]":
-				GUI.altP.setText(retornarValor());
-				GUI.altI.setText(retornarValor());
-				GUI.altD.setText(retornarValor());
-				GUI.velP.setText(retornarValor());
-				GUI.velI.setText(retornarValor());
-				GUI.velD.setText(retornarValor());
-				break;
-			case "[Auto Rover]":
-				GUI.nomeMarcadorTextField.setText(retornarValor());
-				GUI.velMaxTextField.setText(retornarValor());
-				break;
-			default:
-				linha = leitor.nextLine();
-			}
-		}
-
-	}
-
-	public static void criarLogDeErros(StackTraceElement[] elementos) throws IOException {
-		SimpleDateFormat formatar = new SimpleDateFormat("dd-MM-yy 'às' HH:mm:ss");
-		Date data = new Date(System.currentTimeMillis());
-		logErros = new File("mp_erros.log");
-		logErros.createNewFile();
-		escritor = new FileWriter(logErros, true);
-		escritor.write("Log de erros criado na data: " + formatar.format(data) + "\n\n");
-		for (int i = 0; i < elementos.length; i++) {
-			escritor.write(elementos[i].toString() + "\n");
-		}
-		escritor.write("\n");
-		escritor.close();
-	}
-
-	private String retornarValor() {
-		linha = leitor.nextLine();
-		String[] valor = linha.split("=");
-		return valor[1];
-
-	}
-}
->>>>>>> 5fb47a5696ce2a94e7cef178d9d4eb1eda4801d7
